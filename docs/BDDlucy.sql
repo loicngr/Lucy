d9ec71d572760f92e3a81754032fe98554f0bf94
ariale <ariale.arts@gmail.com>
2020-08-27 09:58:13 +0200

    BDD

---
+++ docs/BDDlucy.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Jeu 27 Août 2020 à 09:57
+-- Version du serveur :  5.7.31-0ubuntu0.18.04.1
+-- Version de PHP :  7.2.33-1+ubuntu18.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `BDDlucy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `assoc`
+--
+
+CREATE TABLE `assoc` (
+  `ID_assoc` int(11) NOT NULL,
+  `ID_item` int(11) NOT NULL,
+  `ID_tag` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `item`
+--
+
+CREATE TABLE `item` (
+  `ID_item` int(11) NOT NULL,
+  `ID_room` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `room`
+--
+
+CREATE TABLE `room` (
+  `ID_room` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tag`
+--
+
+CREATE TABLE `tag` (
+  `ID_tag` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `assoc`
+--
+ALTER TABLE `assoc`
+  ADD PRIMARY KEY (`ID_assoc`),
+  ADD KEY `ID_item` (`ID_item`),
+  ADD KEY `ID_tag` (`ID_tag`);
+
+--
+-- Index pour la table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`ID_item`),
+  ADD KEY `ID_room` (`ID_room`),
+  ADD KEY `ID_room_2` (`ID_room`);
+
+--
+-- Index pour la table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`ID_room`);
+
+--
+-- Index pour la table `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`ID_tag`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `assoc`
+--
+ALTER TABLE `assoc`
+  MODIFY `ID_assoc` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `item`
+--
+ALTER TABLE `item`
+  MODIFY `ID_item` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `room`
+--
+ALTER TABLE `room`
+  MODIFY `ID_room` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `ID_tag` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `assoc`
+--
+ALTER TABLE `assoc`
+  ADD CONSTRAINT `item_tag` FOREIGN KEY (`ID_item`) REFERENCES `item` (`ID_item`),
+  ADD CONSTRAINT `tag_item` FOREIGN KEY (`ID_tag`) REFERENCES `tag` (`ID_tag`);
+
+--
+-- Contraintes pour la table `room`
+--
+ALTER TABLE `room`
+  ADD CONSTRAINT `item` FOREIGN KEY (`ID_room`) REFERENCES `item` (`ID_room`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
