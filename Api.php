5a2fbf741f5c579e4b2f95ea6af9a9f6ea4d7555
loicngr <pro.nogierloic@gmail.com>
2020-08-28 10:39:35 +0200

    Add Api file

---
+++ Api.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'App.php';
+
+class Api extends App
+{
+    public function api_getItemsByRoomId($roomID)
+    {
+        return $this->itemsWithTagsRoom($roomID);
+    }
+
+    public function api_deleteItemById($itemId)
+    {
+        return $this->deleteItemById($itemId);
+    }
+}
+
+if (empty($_POST)) {
+    die("false");
+} else if (isset($_POST['type'])) {
+    require_once 'Utils.php';
+
+    $type = Utils::secureString($_POST['type']);
+
+    switch ($type) {
+        case 'getItemsByRoomId': {
+            if (!isset($_POST['roomID'])) die("false");
+            $roomID = (int)$_POST['roomID'];
+
+            $apiClass = new Api();
+            echo json_encode($apiClass->api_getItemsByRoomId($roomID));
+
+            break;
+        }
+        case 'removeItemById': {
+            if (!isset($_POST['itemID'])) die("false");
+            $itemID = (int)$_POST['itemID'];
+
+            $apiClass = new Api();
+            echo json_encode($apiClass->api_deleteItemById($itemID));
+
+            break;
+        }
+        default:
+            die("false");
+    }
+} else {
+    die("false");
+}
